7f211cd09c02fb9d48b103e3d61daf10a76d8b1f
pegasus27 <pega.fk.sus27@gmail.com>
2019-07-25 14:06:59 +0900

    CSVからログを取得して実験Window上に線を描画できるようにできた！！！！！！

--- ResearchWindowGenerator/ResarchWindowTest_Log.xaml.cs
+++ ResearchWindowGenerator/ResarchWindowTest_Log.xaml.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
+//using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows;
@@ -10,6 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
 
 namespace ResearchWindowGenerator
 {
@@ -18,102 +21,39 @@ namespace ResearchWindowGenerator
     /// </summary>
     public partial class ResarchWindowTest_Log : Window
     {
-        private Timer _timer = null;
+        //private Timer _timer = null;
         static double TimeCount = 0.0;
         static List<string[]> lists = new List<string[]>();
         static string csvPath = @"../../../LogFolder/_ResarchWindowText_2019.7.15.14.57.3.csv";
         static int drawCount = 0;
-        static int fileLength = 0;
+        //static int fileLength = 0;
         static int MAXCOLUMN;
-        static int MAXLINE;
+        //static int MAXLINE;
         static double[][] csvContents = new double[1000][];
-
         //TODO : できるならLISTのLISTにしたい　https://teratail.com/questions/40608
         static List<double>[] csvContentsList = new List<double>[100000];
-
-        /*
-         List<Data>[] sample = new List<Data>[3];
-sample[0] = new List<Data>();
-sample[1] = new List<Data>();
-             */
-
-        //private static ArrayList csvContents = new ArrayList();
 
         public ResarchWindowTest_Log()
         {
             InitializeComponent();
-            // タイトルバーとの境界線を表示しない
-            //this.WindowStyle = WindowStyle.None;
             this.WindowStyle = WindowStyle.SingleBorderWindow;
             //最大化表示
             this.WindowState = WindowState.Maximized;
-
-
-
-            //ReadCsv();
-            //MAXCOLUMN = Lookup_MAXCOLUMN();
-            //MAXLINE = Lookup_MAXLINE();
-            //csvContentsList = new List<double>[MAXCOLUMN];
             CsvReader();
-            //Console.WriteLine(lists.Count());
-            //csvContents.
-
             //ログを再生するようにする
             //StartTimer();
-
-
             DrawLogLine();
-
-
-
         }
-
-        public int Lookup_MAXCOLUMN()
-        {
-            int column = 0;
-
-            // csvファイルを開く
-            using (var sr = new System.IO.StreamReader(csvPath))
-            {
-                // ストリームの末尾まで繰り返す
-
-                while (!sr.EndOfStream)
-                {
-                    column++;
-                }
-            }
-
-
-            return column;
-        }
-
-
-
-        public int Lookup_MAXLINE()
-        {
-            int line = 0;
-
-            return line;
-        }
-
-
-
-        
 
         public void CsvReader()
         {
-            //int[][] csv_array = new int[MAXCOLUMN][];
-            //double[][] csv_array = new double[1000][];
-            
             int count = 0;
-
             try
             {
                 // csvファイルを開く
                 using (var sr = new System.IO.StreamReader(csvPath))
                 {
                     // ストリームの末尾まで繰り返す
-                    
                     while (!sr.EndOfStream)
                     {
                         // ファイルから一行読み込む
@@ -127,31 +67,19 @@ sample[1] = new List<Data>();
                         }
                         System.Console.WriteLine();
                         Console.WriteLine(values[0]);
-                        //Console.WriteLine(values[0].GetType());
                         Console.WriteLine(values[1]);
-                        //Console.WriteLine(values[1].GetType());
                         Console.WriteLine(values[2]);
-                        //Console.WriteLine(values[2].GetType());
-                        //lists.AddRange(values);
-                        //Console.WriteLine(lists);
-                        //TODO : CSVファイルの読み込みはできているのに中身の処理がうまくいっていないので考える
-                        //lists.AddRange(values.ToList);
-                        //int counts = 0;
-
+ 
                         csvContentsList[count] = new List<double>();
                         foreach (var value in values)
                         {
                             Console.WriteLine(value.Length);
                             Console.WriteLine(Convert.ToDouble(value));
-                            //csv_array[count][counts] = Convert.ToDouble(value);
                             Console.WriteLine("count =" + count + ", counts = " + count + ", value = " + value);
                             Console.WriteLine("ここからcsv_arrayの中身");
-                            //Console.WriteLine("csv_array" + csv_array);
-                            //csv_array[count][counts] = 0.1111111111111/*Convert.ToDouble(value)*/;
-                            //Console.WriteLine(csv_array[count][counts]);
                             csvContentsList[count].Add(Convert.ToDouble(value));
 
-                            //Console.WriteLine(csvContentsList.);
+                        
                         }
 
                         Console.WriteLine("確認！！！！");
@@ -161,28 +89,7 @@ sample[1] = new List<Data>();
                             
                             Console.WriteLine(r);
                         }
-
-                        /*
-                         * foreach (Dictionary<string, string>element in result) {
-   // elementに上から順にresultの要素が入る
-}
-                         * 
-                         * 
-                         * 
-                         static List<double>[] csvContentsList = new List<double>[1000];
-                         
-                         */
-
-
-                        /*
-                        csv_array[count][0] = double.Parse(values[0]);
-                        Console.WriteLine(csv_array[count][0]);
-                        csv_array[count][1] = double.Parse(values[1]);
-                        Console.WriteLine(csv_array[count][1]);
-                        csv_array[count][2] = double.Parse(values[2]);
-                        Console.WriteLine(csv_array[count][2]);*/
                         count++;
-                        //counts++;
                     }
                     MAXCOLUMN = count;
                 }
@@ -194,28 +101,9 @@ sample[1] = new List<Data>();
             }
 
             Console.WriteLine("ここから中身の確認");
-            /*
-            foreach (double[] a in list)
-            {
-                Console.WriteLine(a);
-
-            }*/
-            
-
-
-
-
-            //return csv_array;
         }
 
-
-
-
-
-
-
         /*
-
         //https://moewe-net.com/csharp/forms-timer
         private void StartTimer()
         {
@@ -238,50 +126,26 @@ sample[1] = new List<Data>();
         */
         private void TickHandler(object sender, EventArgs e)
         {
-
-            //@"../../../LogFolder/_ResarchWindowText_2019.7.15.14.57.3.csv"
-
-            //Console.WriteLine(DateTime.Now.ToString());
-            //マウスカーソルの座標を取得
-            //int x = System.Windows.Forms.Cursor.Position.X;
-            //int y = System.Windows.Forms.Cursor.Position.Y;
-            //Console.WriteLine(TimeCount + "," + x + "," + y);
-            /*
-            StreamReader sr = new StreamReader(@"../../../LogFolder/_ResarchWindowText_2019.7.15.14.57.3.csv");
-
-            TimeCount += 0.25;
-            Logger.SaveMouseCursorPosition(TimeCount, x, y);
-            */
-
             // コンソールに出力する
             TimeCount += 0.25;
             drawCount += 1;
-            //Console.WriteLine(lists[drawCount]);
-            //Console.WriteLine(lists.IndexOf(TimeCount.ToString()));
-
         }
-
-
 
         public void DrawLogLine()
         {
-            //static List<double>[] csvContentsList = new List<double>[100000];
-            /*
-            for(int i = 0; i< MAXCOLUMN; i++)
+            Polyline polyline = new Polyline();
+            polyline.Stroke = Brushes.Black;
+
+            for (int i = 0; i < MAXCOLUMN; i++)
             {
                 Console.WriteLine("csvContentsList[" + i + "]");
-                
-                Console.WriteLine("時間 : " + csvContentsList[i].ToArray()[0] +"秒 //" + "x座標" + csvContentsList[i].ToArray()[1] + " //" + "y座標" + csvContentsList[i].ToArray()[2]);
-            }*/
-            /*
-            PaintEventArgs e;
-            Pen blackPen = new Pen(Color.Black, 3);
-            e.Graphics.DrawLine(blackPen, (float)100, (float)100, (float)800, (float)800);
-            */
 
+                Console.WriteLine("時間 : " + csvContentsList[i].ToArray()[0] + "秒 //" + "x座標" + csvContentsList[i].ToArray()[1] + " //" + "y座標" + csvContentsList[i].ToArray()[2]);
+                polyline.Points.Add(new Point(csvContentsList[i].ToArray()[1], csvContentsList[i].ToArray()[2]));
+            }
+            
+            mycanvas.Children.Add(polyline);
 
         }
-
-
     }
 }
